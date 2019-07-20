a8ae6f4281dc2ea9053d29cb8b73310544b8f9a2
Sandys Nunes <sandysnunes@gmail.com>
2019-07-19 22:26:49 -0400

    Logistic Regression

--- src/main/scala/com/sandys/ReadCsvWithCache.scala
+++ src/main/scala/com/sandys/ReadCsvWithCache.scala
@@ -1,19 +1,18 @@
 package com.sandys
 
-import org.apache.spark.ml.feature.{OneHotEncoder, StringIndexer}
 import org.apache.spark.mllib.linalg
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{Encoders, SparkSession}
+import org.apache.spark.sql.{Row, SparkSession}
+
+import scala.util.matching.Regex
 
 object ReadCsvWithCache {
 
   private val PassengerId = "PassengerId"
   private val Survived = "Survived"
-  //private val SurvivedIndex = Survived + "Index"
   private val Pclass = "Pclass"
-  //private val PclassIndex = Pclass + "Index"
   private val Name = "Name"
   private val Sex = "Sex"
   private val Age = "Age"
@@ -24,40 +23,40 @@ object ReadCsvWithCache {
   private val Cabin = "Cabin"
   private val Embarked = "Embarked"
 
-  private val categoricalColumns  = Seq(Survived, Pclass, Sex, Age, Embarked)
-
-  private val fieldNames = Seq(PassengerId, Survived, Pclass, Name, Sex, Age, SibSp, Parch, Ticket, Fare, Cabin, Embarked)
-
   def apply(spark: SparkSession, path: String): RDD[LabeledPoint] = {
-    spark.read
+
+    val df = spark.read
       .option("header", "true")
       .csv(path)
-      .rdd
+
+    df.rdd
+      .filter( r => r.getAs(Age) != null)
+      .filter( r => r.getAs(Embarked) != null)
+      .filter( r => r.getAs(Fare) != null)
       .map(row => {
-        val valueByField = row.getValuesMap(fieldNames)
-
-        val _class = valueByField(Survived).asInstanceOf[String].toDouble
-        val vectorFeatures = contructVector(valueByField)
-
-        LabeledPoint(_class, vectorFeatures)
+        LabeledPoint(row.getAs[String](Survived).toDouble, contructVector(row))
       })
       .cache()
   }
 
 
-  private def contructVector(valueByField: Map[String, Nothing]): linalg.Vector = {
+  private def contructVector(row: Row): linalg.Vector = {
+    /*val pattern = new Regex("")*/
+
     Vectors.dense(
-      valueByField(Pclass).asInstanceOf[String].toDouble,
-      valueByField(Sex).asInstanceOf[String] match {
+      row.getAs[String](Pclass).toDouble,
+      row.getAs[String](Sex) match {
         case "female" => 0.0
         case "male" => 1.0
       },
-      valueByField(Age).asInstanceOf[String].toDouble,
-      valueByField(SibSp).asInstanceOf[String].toDouble,
-      valueByField(Parch).asInstanceOf[String].toDouble,
-      valueByField(Fare).asInstanceOf[String].toDouble,
-      //valueByField(Cabin).asInstanceOf[String].toDouble
-      valueByField(Embarked).asInstanceOf[String] match {
+      row.getAs[String](Age).toDouble,
+      row.getAs[String](SibSp).toDouble,
+      row.getAs[String](Parch).toDouble,
+      row.getAs[String](Fare).toDouble,
+     /* Some(row.getAs[String](Cabin))
+        .map(c => pattern.findAllIn(c).group(1))
+        .getOrElse(0.0),*/
+      row.getAs[String](Embarked) match {
         case "C" => 0.0
         case "S" => 1.0
         case "Q" => 2.0
@@ -65,3 +64,5 @@ object ReadCsvWithCache {
     )
   }
 }
+
+
--- src/main/scala/com/sandys/TitanicDisaster.scala
+++ src/main/scala/com/sandys/TitanicDisaster.scala
@@ -1,7 +1,8 @@
 package com.sandys
 
-import org.apache.spark.mllib.classification.{SVMModel, SVMWithSGD}
-import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.mllib.classification.{LogisticRegressionModel, LogisticRegressionWithLBFGS, SVMModel, SVMWithSGD}
+import org.apache.spark.mllib.evaluation.{BinaryClassificationMetrics, MulticlassMetrics}
+import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.sql.{DataFrame, Encoders, SQLContext, SparkSession}
@@ -12,36 +13,27 @@ import org.apache.spark.sql.{DataFrame, Encoders, SQLContext, SparkSession}
 object TitanicDisaster extends App {
 
   val trainingPath = "src/main/resource/train.csv"
-  val testPath = "src/main/resource/train.csv"
+  val testPath = "src/main/resource/test.csv"
 
-  val conf: SparkConf = new SparkConf()
-    .setAppName("TitanicDisaster")
-    .setMaster("local[*]")
+  val conf: SparkConf = new SparkConf().setAppName("TitanicDisaster").setMaster("local[*]")
 
   val session: SparkSession = SparkSession.builder().config(conf).getOrCreate()
-  val sc: SparkContext = session.sparkContext
-  val sqlContext: SQLContext = session.sqlContext
-
-  import org.apache.spark.sql.functions._
-  import sqlContext.implicits._
 
   val training = ReadCsvWithCache(session, trainingPath)
   val test = ReadCsvWithCache(session, testPath)
 
 
-  val model: SVMModel = SVMWithSGD.train(training, 100)
+  val model = new LogisticRegressionWithLBFGS().setNumClasses(2).run(training)
 
-  model.clearThreshold()
-
-  val scoreAndLabels = test.map { point =>
-    val score = model.predict(point.features)
-    (score, point.label)
+  // Compute raw scores on the test set.
+  val predictionAndLabels = test.map { case LabeledPoint(label, features) =>
+    val prediction = model.predict(features)
+    (prediction, label)
   }
 
   // Get evaluation metrics.
-  val metrics = new BinaryClassificationMetrics(scoreAndLabels)
-  val auROC = metrics.areaUnderROC()
+  val metrics = new MulticlassMetrics(predictionAndLabels)
+  println(s"Accuracy = ${metrics.accuracy}")
 
-  println(s"Area under ROC = $auROC")
 
 }
